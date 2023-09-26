40cbfa26f634566db06f28d6e67a8dae9e54bb01
nisakib07 <websakib07@gmail.com>
2023-09-26 22:52:58 +0600

    statistics done

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,5 +1,122 @@
+// import { useEffect, useState } from "react";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js/auto";
+// import { Pie } from "react-chartjs-2";
+
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const Statistics = () => {
+//   const [allDonations, setAllDonations] = useState([]);
+
+//   useEffect(() => {
+//     const donatedItemsFromLocalStorage = JSON.parse(
+//       localStorage.getItem("donatedItems")
+//     );
+//     setAllDonations(donatedItemsFromLocalStorage);
+//   }, []);
+
+//   const myDonation = allDonations.length;
+//   const remainingDonation = 12 - myDonation;
+
+//   const data = {
+//     datasets: [
+//       {
+//         data: [myDonation, remainingDonation],
+//       },
+//     ],
+//     labels: ["Your Donation", "Total Donation"],
+//   };
+//   const options = {};
+
+//   return (
+//     <div className="w-52">
+//       <Pie data={data} options={options}></Pie>
+//     </div>
+//   );
+// };
+
+// export default Statistics;
+
+import { useEffect, useState } from "react";
+import { PieChart, Pie, Cell } from "recharts";
+
 const Statistics = () => {
-  return <div>This is statistics</div>;
+  const [allDonations, setAllDonations] = useState([]);
+
+  useEffect(() => {
+    const donatedItemsFromLocalStorage = JSON.parse(
+      localStorage.getItem("donatedItems")
+    );
+    setAllDonations(donatedItemsFromLocalStorage);
+  }, []);
+
+  const myDonation = allDonations.length;
+  const remainingDonation = 12 - myDonation;
+  const data = [
+    { name: "myDonation", value: myDonation },
+    { name: "remainingDonation", value: remainingDonation },
+  ];
+
+  const COLORS = ["#00C49F", "#FF444A", "#FFBB28"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central">
+        {`${(percent * 100).toFixed(2)}%`}
+      </text>
+    );
+  };
+  return (
+    <div>
+      <div className="flex justify-center">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={130}
+            fill="#8884d8"
+            dataKey="value">
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </div>
+      <div className="flex justify-center gap-14">
+        <div className="flex items-center gap-4">
+          <h4>Your Donation</h4>
+          <div className="w-[100px] h-3 bg-[#00C49F;]"></div>
+        </div>
+        <div className="flex items-center gap-4">
+          <h4>Your Donation</h4>
+          <div className="w-[100px] h-3 bg-[#FF444A]"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,9 @@
 const Banner = () => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const searchText = e.target.searchText.value;
+  };
+
   return (
     <div>
       <div
@@ -9,15 +14,10 @@ const Banner = () => {
         }}>
         <div className="hero-overlay opacity-100"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+          <form onSubmit={handleOnSubmit}>
+            <input className="text-black" type="text" name="searchText" />
+            <input className="bg-[blue]" type="submit" value="Search" />
+          </form>
         </div>
       </div>
     </div>
